346fe0766cc93add95bf6e5c0ea0cf047a7dc085
Ανδρέας Κοντός <andreaskontos@ip-192-168-1-214.ec2.internal>
2024-12-06 19:25:19 +0100

    fix lambda_function error

--- src/lambda_function.js
+++ src/lambda_function.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
       const numbers = fileContents.numbers || []; // Ensure numbers is an array or default to an empty array
       const sum = numbers.reduce((acc, num) => acc + num, 0);
       console.log(`The sum of all the numbers in the JSON file is: ${sum}`);
-      console, log(`FILE: ${objectKey} | SUM: ${sum}`);
+      console.log(`FILE: ${objectKey} | SUM: ${sum}`);
     }
   } catch (err) {
     console.error(`Error while triggering Lambda function: ${err?.message}`);
